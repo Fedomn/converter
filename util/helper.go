2ff04faed030fa50124f689246fb7b7659eed9a6
Fedomn <shadowforfiend@gmail.com>
2018-05-19 22:03:28 +0800

    init

---
+++ util/helper.go
@@ -0,0 +1,80 @@
+package util
+
+import (
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"testing"
+)
+
+func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d: "+msg+"\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
+
+func Equals(tb testing.TB, msg string, wat, got interface{}) {
+	if !reflect.DeepEqual(wat, got) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d: %s \n\n\twat: %#v\n\n\tgot: %#v\n\n", filepath.Base(file), line, msg, wat, got)
+		tb.FailNow()
+	}
+}
+
+func Ok(tb testing.TB, msg string, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d: %s \n\n unexpected error: %s\n\n", filepath.Base(file), line, msg, err.Error())
+		tb.FailNow()
+	}
+}
+
+type validator func(interface{}) bool
+
+func Contains(source interface{}, find interface{}) bool {
+	sourceVal := reflect.ValueOf(source)
+	if sourceVal.Type().Kind() != reflect.Slice {
+		return false
+	}
+
+	for i := 0; i < sourceVal.Len(); i++ {
+		val := sourceVal.Index(i).Interface()
+		if val == find {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsBy(source interface{}, fn validator) bool {
+	sourceVal := reflect.ValueOf(source)
+	if sourceVal.Type().Kind() != reflect.Slice {
+		return false
+	}
+
+	for i := 0; i < sourceVal.Len(); i++ {
+		val := sourceVal.Index(i).Interface()
+		if match := fn(val); match == true {
+			return true
+		}
+	}
+	return false
+}
+
+func FindBy(source interface{}, fn validator) (interface{}, error) {
+	sliceVal := reflect.ValueOf(source)
+	if sliceVal.Type().Kind() != reflect.Slice {
+		return nil, fmt.Errorf("parameter 1 must be a slice")
+	}
+
+	for i := 0; i < sliceVal.Len(); i++ {
+		val := sliceVal.Index(i).Interface()
+		if match := fn(val); match == true {
+			return val, nil
+		}
+	}
+	return nil, nil
+}
---
+++ util/helper_test.go
@@ -0,0 +1,74 @@
+package util_test
+
+import (
+	. "fedomn/converter/util"
+	"fmt"
+	"testing"
+)
+
+func TestContains(t *testing.T) {
+	var disTest = []struct {
+		source interface{}
+		find   interface{}
+		wat    bool
+	}{
+		{[]string{"1", "2", "3"}, "1", true},
+		{[]int{1, 2, 3}, 0, false},
+		{1, 0, false},
+	}
+
+	for _, tt := range disTest {
+		got := Contains(tt.source, tt.find)
+		Equals(t, fmt.Sprintf("source: %+v", tt.source), tt.wat, got)
+	}
+}
+
+func TestContainsBy(t *testing.T) {
+	var disTest = []struct {
+		source    interface{}
+		wat       bool
+		validator func(interface{}) bool
+	}{
+		{[]string{"1", "2", "3"}, true, func(val interface{}) bool {
+			return val.(string) == "3"
+		}},
+		{[]int{1, 2, 3}, false, func(val interface{}) bool {
+			return val.(int) == 0
+		}},
+		{1, false, func(val interface{}) bool {
+			return false
+		}},
+	}
+
+	for _, tt := range disTest {
+		got := ContainsBy(tt.source, tt.validator)
+		Equals(t, fmt.Sprintf("source: %+v", tt.source), tt.wat, got)
+	}
+}
+
+func TestFindBy(t *testing.T) {
+	var disTest = []struct {
+		source    interface{}
+		wat       interface{}
+		validator func(interface{}) bool
+	}{
+		{[]string{"1", "2", "3"}, "3", func(val interface{}) bool {
+			return val.(string) == "3"
+		}},
+		{[]int{1, 2, 3}, nil, func(val interface{}) bool {
+			return val.(int) == 0
+		}},
+		{1, fmt.Errorf("parameter 1 must be a slice"), func(val interface{}) bool {
+			return false
+		}},
+	}
+
+	for _, tt := range disTest {
+		got, err := FindBy(tt.source, tt.validator)
+		if err != nil {
+			Equals(t, fmt.Sprintf("source: %+v", tt.source), tt.wat, err)
+		} else {
+			Equals(t, fmt.Sprintf("source: %+v", tt.source), tt.wat, got)
+		}
+	}
+}
